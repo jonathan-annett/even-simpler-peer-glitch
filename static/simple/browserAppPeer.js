321dbf443e98b8f6edaa74bf5f2180b7dc98e4c7
Jonathan <jonathan.max.annett@gmail.com>
2023-04-11 11:33:08 +1000

    wip

--- static/simple/browserAppPeer.js
+++ static/simple/browserAppPeer.js
@@ -64,6 +64,9 @@ function browserAppPeer(button, labels) {
 
         const sdp3 = sdp2.replace(/a=ice-ufrag\:.*(?=(\r|\n))/g,'');
         const sdp4 = sdp3.replace(/a=ice-pwd\:.*(?=(\r|\n))/g,'');
+        const sdp5 = sdp4.replace(/a=fingerprint\:sha-256.*(?=(\r|\n))/g,'');
+
+        
         const sdp5 = sdp4.replace(/\s*/g,'');
 
         sha256Hash(sdp5,function(err,digest){
