255d8bad3605a130d7b1fc31bb8edfcd92e31462
Jonathan <jonathan.max.annett@gmail.com>
2023-04-20 15:40:47 +1000

    added test code for ws peer

--- static/test-frame.js
+++ static/test-frame.js
@@ -24,9 +24,10 @@ SOFTWARE.
 
 */
 
-/* global evenSimplerPeer */   
+/* global evenSimplerPeer,evenSimplerWSPeer */   
 
       let peer,
+          peerViaWsBtn =  document.querySelector("#peerViaWsBtn"),
           logger = document.querySelector("pre"),
           json = document.querySelector("textarea"),
           btn = document.querySelector("button");
@@ -71,5 +72,40 @@ SOFTWARE.
              
            }
         };
+
+
+        peerViaWsBtn.onclick = function () {
+          const token = location.search;
+          if (token && token.length === 25 && token.charAt(0)==='?') {
+             const own_id = token.substr(1,12);
+             const peer_id = token.substr(13,12);
+             if (evenSimplerPeer.validateId(own_id)) {
+                if (evenSimplerPeer.validateId(peer_id)) {
+                   if (peer) peer.destroy();
+
+                   peer = evenSimplerWSPeer({own_id, peer_id });
+                   peer.on('data',function(msg){
+                    logger.innerHTML += "in: "+JSON.stringify(msg,undefined,4)+"\n";
+                   });
+
+                   btn.onclick = function(){
+                    try {
+                      let msg = JSON.parse(json.value);
+                      peer.send(msg);
+                      json.value = JSON.stringify(msg,undefined,4);
+                    } catch (e) {
+                      
+                    }
+                 };
+         
+
+                }
+             }
+          }
+          
+        }
       };
+
+
+
       
